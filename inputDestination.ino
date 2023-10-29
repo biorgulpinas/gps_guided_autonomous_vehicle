b931e2dccb68f40420c98c1ad518229f2911c204
biorgulpinas <149315459+biorgulpinas@users.noreply.github.com>
2023-10-30 00:02:58 +0100

    Update inputDestination.ino

--- inputDestination.ino
+++ inputDestination.ino
@@ -4,32 +4,29 @@ void inputDestination() {       // The function that will allow us to input our 
   bool Quit = true;
 
   while ( Quit == true ) {
-    Serial2.println("Destination Menu");
-    Serial2.println("Please input a destination (latitude, longitude) when prompted.");
-    Serial2.println("Warning: Coordinate will be off by a very small amount due to Arduino limitations with float precision");
+    Serial2.println("Menu celu");
+    Serial2.println("Po wyświetleniu monitu wprowadź miejsce docelowe.");
     delay(5000);
-    Serial2.println("Input your latitude coordinate: ");
+    Serial2.println("Podaj szerokość geograficzną: ");
     latitB = stringToFloatConversion();
-    Serial2.println("Input your longitude coordinate: ");
+    Serial2.println("Podaj długość geograficzną: ");
     lonB = stringToFloatConversion();
-    Serial2.println("Your destination coordinate is: ");
-    Serial2.print("Latitude: ");
+    Serial2.println("Twoje współrzędne docelowe to: ");
     Serial2.println(latitB, 6);
-    Serial2.print("Longitude: ");
     Serial2.println(lonB, 6);
 
-    Serial2.println("Is this destination correct?   'y': yes   'n': no   ");  // This checks if our data is correct
+    Serial2.println("Zatwierdzić 't': tak  'n': nie  ");              // This checks if our data is correct
     while (Serial2.available() == 0) {}                                       // Wait for user input in terminal
     d = Serial2.read();
     if (d == 'y') {                                                           // If our destination coordinates are correct
-      Serial2.println("Destination confirmed. Proceeding to main menu.");
+      Serial2.println("Powrót do głównego menu");
       Quit = false;                                                           // ...we can quit the menu by breaking out of
     }                                                                         // the entire while loop (and function)
     else if (d == 'n') {                                                      // Else, we stay in the menu until valid coordinates
-      Serial2.println("Destination not confirmed. Going back to Destination menu");
+      Serial2.println("Ponowny wybór celu");
     }
     else {
-      Serial2.println("Destination isn't clear. Going back to Destination menu");
+      Serial2.println("Zły wybór. Ponowny wybór celu");
     }
 
   }
