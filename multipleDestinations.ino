3537840d7151b451285938dcd240c1effbafd683
biorgulpinas <149315459+biorgulpinas@users.noreply.github.com>
2023-10-30 07:43:47 +0100

    Update multipleDestinations.ino

--- multipleDestinations.ino
+++ multipleDestinations.ino
@@ -3,17 +3,12 @@ void multipleDestinations() {           // The menu that will run infinitely in 
   char g;
   bool Quit = false;
 
-  Serial2.println("Multiple Destinations Menu");
-  Serial2.println("The vehicle can travel up to three destinations in sequential order: Destination 1 --> 2 --> 3");
+  Serial2.println("Menu kilku celów");
+  Serial2.println("Możesz wyznaczyć trzy kolejne cele");
   delay(1000);
-  Serial2.println("Please input up to THREE destinations (latitude, longitude) when prompted.");
-  delay(1000);
-  Serial2.println("Warning: Coordinates will be off by a very small amount due to Arduino limitations with float precision");
-  Serial2.println("Please enter one of the following numbers or characters to access each option");
-  Serial2.println("Note: Enter 'd' to display menu options");
-  delay(1000);
-  Serial2.println("'1': Destination 1   '2': Destination 2   '3': Destination 3   ");
-  Serial2.println("'e': Exit menu   's': Show destinations   'd': Display options");
+  Serial2.println("Wprowadź odpowiedni znak, żeby uzyskać dostęp do wybranej opcji.");
+  Serial2.println("'1': Cel 1   '2': Cel 2   '3': Cel 3   ");
+  Serial2.println("'e': Wyjdź z menu  's': Pokaż cele  'd': Wyświetl opcje");
   delay(50);
 
   while (!Quit) {                   // As long as Quit isn't true, this switch/case menu will run infinitely
@@ -21,63 +16,51 @@ void multipleDestinations() {           // The menu that will run infinitely in 
       g = Serial2.read();
       switch (g) {
         case '1':     // Destination 1
-          Serial2.println("Input your first destination latitude coordinate: ");
+          Serial2.println("Wprowadź współrzędną szerokości geograficznej pierwszego miejsca docelowego:");
           latitB = stringToFloatConversion();
-          Serial2.println("Input your first destination longitude coordinate: ");
+          Serial2.println("Wprowadź współrzędną długości graficznej pierwszego miejsca docelowego:");
           lonB = stringToFloatConversion();
           delay(50);
-          Serial2.println("Your first destination coordinate is: ");
-          Serial2.print("Latitude: ");
+          Serial2.println("Współrzędne pierwszego celu:");
           Serial2.println(latitB, 6);
-          Serial2.print("Longitude: ");
           Serial2.println(lonB, 6);
           break;
         case '2':     // Destination 2
-          Serial2.println("Input your second destination latitude coordinate: ");
+          Serial2.println("Wprowadź współrzędną szerokości geograficznej drugiego miejsca docelowego:");
           latitC = stringToFloatConversion();
-          Serial2.println("Input your second destination longitude coordinate: ");
+          Serial2.println("Wprowadź współrzędną długości graficznej drugiego miejsca docelowego:");
           lonC = stringToFloatConversion();
           delay(50);
-          Serial2.println("Your second destination coordinate is: ");
-          Serial2.print("Latitude: ");
+          Serial2.println("Współrzędne drugiego celu:");
           Serial2.println(latitC, 6);
-          Serial2.print("Longitude: ");
           Serial2.println(lonC, 6);
           break;
         case '3':     // Destination 3
-          Serial2.println("Input your third destination latitude coordinate: ");
+          Serial2.println("Wprowadź współrzędną szerokości geograficznej trzeciego miejsca docelowego:");
           latitD = stringToFloatConversion();
-          Serial2.println("Input your third destination longitude coordinate: ");
+          Serial2.println("Wprowadź współrzędną długości graficznej trzeciego miejsca docelowego:");
           lonD = stringToFloatConversion();
           delay(50);
-          Serial2.println("Your third destination coordinate is: ");
-          Serial2.print("Latitude: ");
+          Serial2.println("Współrzędne trzeciego celu:");
           Serial2.println(latitD, 6);
-          Serial2.print("Longitude: ");
           Serial2.println(lonD, 6);
           break;
         case 'e':     // Letter e
-          Serial2.println("Exiting the Multiple Destinations menu.");   // Quit the menu
+          Serial2.println("Wychodzenie z menu Wielu celów.");   // Quit the menu
           Quit = true;
           break;
         case 's':     // Letter s
-          Serial2.println("These are your current destinations");
-          Serial2.println("Your first destination coordinate is: ");
-          Serial2.print("Latitude: ");
+          Serial2.println("Aktualne cele");
+          Serial2.println("Cel 1: ");
           Serial2.println(latitB, 6);
-          Serial2.print("Longitude: ");
           Serial2.println(lonB, 6);
           delay(50);
-          Serial2.println("Your second destination coordinate is: ");
-          Serial2.print("Latitude: ");
+          Serial2.println("Cel 2: ");
           Serial2.println(latitC, 6);
-          Serial2.print("Longitude: ");
           Serial2.println(lonC, 6);
           delay(50);
-          Serial2.println("Your third destination coordinate is: ");
-          Serial2.print("Latitude: ");
+          Serial2.println("Cel 3: ");
           Serial2.println(latitD, 6);
-          Serial2.print("Longitude: ");
           Serial2.println(lonD, 6);
           break;
         case 'd':     // Letter d
