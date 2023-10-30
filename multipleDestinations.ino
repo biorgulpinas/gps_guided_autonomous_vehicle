fbd864f1e698fa492ea81082cbf6b52b1cd2d7b2
biorgulpinas <149315459+biorgulpinas@users.noreply.github.com>
2023-10-30 18:24:21 +0100

    Update multipleDestinations.ino

--- multipleDestinations.ino
+++ multipleDestinations.ino
@@ -3,7 +3,7 @@ void multipleDestinations() {           // The menu that will run infinitely in 
   char g;
   bool Quit = false;
 
-  Serial2.println("Menu kilku celów");
+  Serial2.println("Menu wielu celów");
   Serial2.println("Możesz wyznaczyć trzy kolejne cele");
   delay(1000);
   Serial2.println("Wprowadź odpowiedni znak, żeby uzyskać dostęp do wybranej opcji.");
@@ -46,7 +46,7 @@ void multipleDestinations() {           // The menu that will run infinitely in 
           Serial2.println(lonD, 6);
           break;
         case 'e':     // Letter e
-          Serial2.println("Wychodzenie z menu Wielu celów.");   // Quit the menu
+          Serial2.println("Wychodzenie z menu wielu celów.");   // Quit the menu
           Quit = true;
           break;
         case 's':     // Letter s
@@ -64,18 +64,11 @@ void multipleDestinations() {           // The menu that will run infinitely in 
           Serial2.println(lonD, 6);
           break;
         case 'd':     // Letter d
-          Serial2.println("Displaying Multiple Destinations Menu options");
-          Serial2.println("Multiple Destinations Menu");
-          Serial2.println("The vehicle can travel up to three destinations in sequential order: Destination 1 --> 2 --> 3");
+          Serial2.println("Opcje menu wielu celów");
           delay(1000);
-          Serial2.println("Please input up to THREE destinations (latitude, longitude) when prompted.");
-          delay(1000);
-          Serial2.println("Warning: Coordinates will be off by a very small amount due to Arduino limitations with float precision");
-          Serial2.println("Please enter one of the following numbers or characters to access each option");
-          Serial2.println("Note: Enter 'd' to display menu options");
-          delay(1000);
-          Serial2.println("'1': Destination 1   '2': Destination 2   '3': Destination 3   ");
-          Serial2.println("'e': exit menu   's': Show destinations   'd': Display options");
+          Serial2.println("Wprowadź odpowiedni znak, żeby uzyskać dostęp do wybranej opcji.");
+          Serial2.println("'1': Cel 1   '2': Cel 2   '3': Cel 3   ");
+          Serial2.println("'e': Wyjdź z menu  's': Pokaż cele  'd': Wyświetl opcje");
           delay(50);
           break;
       }
